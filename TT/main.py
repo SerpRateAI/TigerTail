fe684d20439b1a9079f1f28dba03f3dd94482d44
John Aiken <johnm.aiken@gmail.com>
2024-10-09 14:17:02 +0200

    Update main.py

--- TT/main.py
+++ TT/main.py
@@ -1,5 +1,21 @@
-def do():
+from window_functions import *
+
+class ts:
+  def __init__(self, data):
+    self.data = data
+
+def window:
   pass
+
+class EventSeries(ts):
+  pass
+
+class TimeSeries(ts):
+  pass
+
+class TTDataFrame:
+  pass
+        
 
 if __name__ == '__main__':
   print('hello there! ~ obi-wan kenobi')
